499cb98dfa67a57e149b725729fb4bba89309dc7
Brady Newhard <brady.russell@gmail.com>
2025-03-14 03:08:27 -0400

    added game links and Landing Page

--- src/components/Games/Games.jsx
+++ src/components/Games/Games.jsx
@@ -1,7 +1,53 @@
+import { Link } from 'react-router-dom';
+
 const Games = () => {
   return (
     <main>
       <h1>Games & Activities</h1>
+      
+      <section>
+        <a 
+          href="https://brady-newhard.github.io/little-mermaid-match-game/" 
+          target="_blank" 
+          rel="noopener noreferrer"
+        >
+          The Little Mermaid Match Game
+        </a>
+        <br />
+        <a 
+          href="https://mendezugenbuehler.github.io/memory-game/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Power Up via Memory Game
+        </a>
+        <br />
+        <a href="https://brady-newhard.github.io/tic-tac-toe/"
+        target="_blank"
+        rel="noopener noreferrer"
+        >
+          Tic Tac Toe
+        </a>
+        <br />
+        <a href="https://licenseplates.app/"
+        target="_blank"
+        rel="noopener noreferrer"
+        >
+          The License Plate Game
+        </a>
+
+      </section>
+
+      <section>
+        <h2>Account Access</h2>
+        <Link to="/sign-up">
+          <button>Sign Up</button>
+        </Link>
+        {' '}
+        <Link to="/sign-in">
+          <button>Sign In</button>
+        </Link>
+      </section>
     </main>
   );
 };
--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -1,8 +1,20 @@
+import { Link } from 'react-router-dom';
+
 const Landing = () => {
   return (
     <main>
-      <h1>Hello, you are on the landing page for visitors.</h1>
-      <p>Sign up now, or sign in to see your super secret dashboard!</p>
+      <h1>Welcome to the KidStop—where families roam free, paws explore happily, and parents recharge effortlessly. Discover playgrounds, dog parks, endless games, and curated media—all in one joyful journey. Your family adventure, simplified.</h1>
+      <p>Sign up now—or sign in—to unlock your well-deserved escape on the super-secret dashboard!</p>
+      
+      <div>
+        <Link to="/sign-up">
+          <button>Sign Up</button>
+        </Link>
+        {' '}
+        <Link to="/sign-in">
+          <button>Sign In</button>
+        </Link>
+      </div>
     </main>
   );
 };
